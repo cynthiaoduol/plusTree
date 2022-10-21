ce2df867229ee6a4a53f7c17823850ab327571f5
cynthiaoduol <cynthiaobu940@gmail.com>
2022-10-21 12:22:09 +0300

    initial commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,29 @@
+import { useEffect } from 'react'
 import '../styles/globals.css'
+import Head from 'next/head';
+import Script from 'next/script';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  
+  return (
+    <div>
+      <Head>
+
+        <link
+          rel='stylesheet'
+          href='https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/css/bootstrap.min.css'
+          referrerPolicy='no-referrer'
+        />
+      </Head>
+
+      <Component {...pageProps} />
+
+      <Script
+        src='https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/js/bootstrap.bundle.min.js'
+        strategy='afterInteractive'
+      />
+    </div>
+  )
 }
 
 export default MyApp
