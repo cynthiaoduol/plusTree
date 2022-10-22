ac3e8dc275773cc008fe905e31e79d9a5951ead5
cynthiaoduol <cynthiaobu940@gmail.com>
2022-10-22 12:18:47 +0300

    create the landing page ui

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,12 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
+import Homepage from './Homepage/Homepage'
 
 export default function Home() {
   return (
     <div>
-     <h1>PlusTree</h1>
+     <Homepage/>
     </div>
   )
 }
