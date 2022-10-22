59c8f5f099dd0978e9767a7236ce46c539e252cb
lulalumala <stephenlumala@gmail.com>
2022-10-22 12:17:37 +0300

    changed the modal component

--- components/modal.js
+++ components/modal.js
@@ -1,49 +1,28 @@
-import { Box, Typography } from "@mui/material"
-import { handleOpen, style } from "@mui/material"
-import { handleClose, open } from "@mui/material"
-import styles from "./modal.module.css"
-import styled from "styled-components"
-
-const Label = styled.label``
-const Input=styled.input``
-
 const Modal = () => {
   return (
     <>
-      <button type="button" data-bs-toggle="modal" data-bs-target="#exampleModal">
-        WE NEED TREES</button>
+<button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#staticBackdrop">
+  Launch static backdrop modal
+</button>
 
-      <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-        <div class="modal-dialog modal-lg" className={styles.Modal}>
-          <div class="modal-content">
-            <div class="modal-header">
-              <h1 class="modal-title fs-5" id="exampleModalLabel">Modal title</h1>
-              <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-            </div>
-            <div class="modal-body">
-              <form>
-                <Label className={styles.label}>First Name: </Label><Input type="text" />
-                <Label className={styles.label}>Last Name: </Label><Input type="text" />
-                <Label className={styles.label}>Phone Number: </Label><Input type="text" />
-                <Label className={styles.label}>Email: </Label><Input type="text" />
-                <Label className={styles.label}>County: </Label><Input type="text" />
-                <Label className={styles.label}>Constituency: </Label><Input type="text" />
-                <Label className={styles.label}>Ward: </Label><Input type="text" />
-                <Label className={styles.label}>LandMark: </Label><Input type="text" />
-                <Label className={styles.label}>Soil type: </Label><Input type="text"/>
-
-
-              </form>
-            </div>
-            <div class="modal-footer">
-              <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-              <button type="button" class="btn btn-primary">Save changes</button>
-            </div>
-          </div>
-        </div>
+<div class="modal fade" id="staticBackdrop" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h1 class="modal-title fs-5" id="staticBackdropLabel">Modal title</h1>
+        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
       </div>
-
-
-    </>)
+      <div class="modal-body">
+        ...
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+        <button type="button" class="btn btn-primary">Understood</button>
+      </div>
+    </div>
+  </div>
+</div>
+    </>
+  )
 }
 export default Modal
--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,12 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
+import Modal from '../components/modal'
 
 export default function Home() {
   return (
     <div>
+      
      <h1>PlusTree</h1>
     </div>
   )
